1f6425aed293c52f669efaf34913ffa9ddd9e39b
numfar85 <numfar85@gmail.com>
2012-09-20 12:02:52 +0200

    Added some comments about what needs to be included

--- pairwiseAlignment.pm
+++ pairwiseAlignment.pm
@@ -3,12 +3,20 @@ package pairwiseAlignment;
 #our @EXPORT_OK = qw(pa);
 use strict;
 use warnings;
-use Exporter;
+#use Exporter;
+
 
 sub pa {
   for my $qi (0..($#_-1)) {
       for my $ri (($qi+1..$#_)) {
-	  qx(nucmer --prefix=temp $_[$qi] $_[$ri]);
+	  my $nucmer_tempy = "nucmer_out_temp".$qi."_".$ri; 
+	  qx(nucmer --prefix="$nucmer_tempy" "$_[$qi]" "$_[$ri]");
+          #get .delta file
+          #delta-filter to remove alignments with low identity
+          #.delta file -> fasta format
+          #Extract consensus and other alignment information (length, identity)
+          #Output extracted information to main/file/stdout
+	  qx(rm -f "$nucmer_tempy.delta");
       }
   }
 }

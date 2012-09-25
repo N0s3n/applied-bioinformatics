e1b34590e3478eb2ca0908c58d3086bf4b602687
numfar85 <numfar85@gmail.com>
2012-09-25 16:30:35 +0200

    Made some comments about what needs to be done. Added a new helper function called get_info() that does not do anything yet.

--- pairwiseAlignment.pm
+++ pairwiseAlignment.pm
@@ -7,18 +7,41 @@ use warnings;
 
 
 sub pa {
-  for my $qi (0..($#_-1)) {
-      for my $ri (($qi+1..$#_)) {
-	  my $nucmer_tempy = "nucmer_out_temp".$qi."_".$ri; 
-	  qx(nucmer --prefix="$nucmer_tempy" "$_[$qi]" "$_[$ri]");
-          #get .delta file
-          #delta-filter to remove alignments with low identity
-          #.delta file -> fasta format
-          #Extract consensus and other alignment information (length, identity)
-          #Output extracted information to main/file/stdout
-	  qx(rm -f "$nucmer_tempy.delta");
-      }
-  }
+    my $minid = 90;
+    my $minlength = 20;
+    my $alignment_p;
+    my $alignment_c;
+
+    for my $qi (0..$#_) {
+	for my $ri (0..$#_) {
+	    if ($qi not $ri) {
+
+		if (($qi != 0) & ($ri != 1)) {
+		    open($fh,'<',$_[$qi])
+		    write($nucmer_tempy.delta);
+		    #Filter out contigs
+                    #get_info($nucmer_tempy);
+		}
+
+		my $nucmer_tempy = "nucmer_out_temp".$qi."_".$ri; 
+		qx(nucmer --prefix="$nucmer_tempy" "$_[$qi]" "$_[$ri]");
+		#A = AiAj
+		#Ai = A_subi
+		#Aj = A_subj
+		#get .delta file
+		#delta-filter to remove alignments with low identity
+                #qx(delta-filter -i="$minid" -l="$minlength" "$nucmer_tempy.delta");
+		#.delta file -> fasta format
+		#Extract consensus and other alignment information (length, identity)
+		#Output extracted information to main/file/stdout
+		qx(rm -f "$nucmer_tempy.delta");
+	    }
+	}
+    }
+}
+
+sub get_info {
+    #get position and contig information from .delta file using regular expressions
 }
 1
 

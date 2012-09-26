e2e7bc59d5cff407472774f6a43e00ca0ebe5991
numfar85 <numfar85@gmail.com>
2012-09-26 16:28:33 +0200

    Renamed get_info to get contigs. Open and reads contig file line by line.

--- pairwiseAlignment.pm
+++ pairwiseAlignment.pm
@@ -4,27 +4,47 @@ package pairwiseAlignment;
 use strict;
 use warnings;
 #use Exporter;
-
+#use File::Slurp;
 
 sub pa {
     my $minid = 90;
     my $minlength = 20;
     my $alignment_p;
     my $alignment_c;
+    my $fh1;
+    my $fh2;
+    my $nucmer_tempy;
+    my $filtered;
+
 
     for my $qi (0..$#_) {
 	for my $ri (0..$#_) {
-	    if ($qi not $ri) {
+
+	   
+		    #open ($fh2,'<',$nucmer_tempy.".delta");
+		    #$assembly = read_file($_[$qi]);
+		    #$delta = read_file($nucmer_tempy.delta);
+
+		    $filtered = get_info($fh1);
+	
+		    #Filter out contigs
+                    #get_info($nucmer_tempy);
+	    if ($qi != $ri) {
 
 		if (($qi != 0) & ($ri != 1)) {
-		    open($fh,'<',$_[$qi])
-		    write($nucmer_tempy.delta);
+		    open(my $fh1, "<",$_[$qi]) 
+			or die "cannot open < ".$_[$qi].": $!";
+		    #open(my $fh2, "<",$nucmer_tempy.".delta") 
+#			or die "cannot open < ".$nucmer_tempy.".delta".": $!";
+
+		    @contigs = get_contigs($fh1);
+	
 		    #Filter out contigs
                     #get_info($nucmer_tempy);
 		}
 
-		my $nucmer_tempy = "nucmer_out_temp".$qi."_".$ri; 
-		qx(nucmer --prefix="$nucmer_tempy" "$_[$qi]" "$_[$ri]");
+		#my $nucmer_tempy = "nucmer_out_temp".$qi."_".$ri; 
+		#qx(nucmer --prefix="$nucmer_tempy" "$_[$qi]" "$_[$ri]");
 		#A = AiAj
 		#Ai = A_subi
 		#Aj = A_subj
@@ -34,16 +54,22 @@ sub pa {
 		#.delta file -> fasta format
 		#Extract consensus and other alignment information (length, identity)
 		#Output extracted information to main/file/stdout
-		qx(rm -f "$nucmer_tempy.delta");
 	    }
 	}
     }
+    #qx(rm -f "$nucmer_tempy.delta");
+    #qx(rm -f "$nucmer_tempy.txt");
+    return $filtered."fi";
 }
 
-sub get_info {
-    #get position and contig information from .delta file using regular expressions
+sub get_contigs {
+    my $contigs;
+
+    for my $fh (@_) {
+	for my $line (<$fh>) {
+	    $contigs .= $line."test"; 
+	}
+    }
+    return $contigs;
 }
 1
-
-
-

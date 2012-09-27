7c62cc0a28e8d7961e9f1a419ee1c5de38fe1b96
numfar85 <numfar85@gmail.com>
2012-09-27 14:44:21 +0200

    get_contigs now returns array containing hashes

--- pairwiseAlignment.pm
+++ pairwiseAlignment.pm
@@ -14,8 +14,8 @@ sub pa {
     my $fh1;
     my $fh2;
     my $nucmer_tempy;
-    my %filtered;
-    my $contigs;
+    my @contigs;
+    #my $contigs;
 
 
     for my $qi (0..$#_) {
@@ -27,16 +27,17 @@ sub pa {
 		    #open ($fh2,'<',$nucmer_tempy.".delta");
 		    #$assembly = read_file($_[$qi]);
 		    #$delta = read_file($nucmer_tempy.delta);
+		    #open(my $fh2, "<",$nucmer_tempy.".delta") 
+#			or die "cannot open < ".$nucmer_tempy.".delta".": $!";
 
-	    %filtered = get_contigs($fh1);
+	    @contigs = get_contigs($fh1);
+            #$A_sub_name = filter(%contigs, $fh2);
 	
-	    #Filter out contigs
-	    #get_info($nucmer_tempy);
 	    if ($qi != $ri) {
 
 		if (($qi != 0) & ($ri != 1)) {
-		    open(my $fh1, "<",$_[$qi]) 
-			or die "cannot open < ".$_[$qi].": $!";
+		    #open(my $fh1, "<",$_[$qi]) 
+		#	or die "cannot open < ".$_[$qi].": $!";
 		    #open(my $fh2, "<",$nucmer_tempy.".delta") 
 #			or die "cannot open < ".$nucmer_tempy.".delta".": $!";
 		    
@@ -62,12 +63,14 @@ sub pa {
     }
     #qx(rm -f "$nucmer_tempy.delta");
     #qx(rm -f "$nucmer_tempy.txt");
-    return %filtered;
+    return @contigs;
 }
 
 #function that goes through the .delta file and stores the contig ids and their positions in a hash.
 sub get_contigs {
-    my %contigs;
+    my @contigs;
+    my %contigs1;
+    my %contigs2;
     my $c1;
     my $c2;
 
@@ -78,29 +81,20 @@ sub get_contigs {
 		$c2 = $2;
 	    }
 	    if ($line =~ /^(\d+)\s(\d+)\s(\d+)\s(\d+)\s\d+\s\d+\s\d+$/) {
-		$contigs{$c1} = ($1,$2);
-		$contigs{$c2} = ($3,$4);
+		$contigs1{$c1} = [$1,$2];
+		$contigs2{$c2} = [$3,$4];
 	    }
 	}
     }
-    return %contigs;
+
+    $contigs[0] = \%contigs1;
+    $contigs[1] = \%contigs2;
+    return @contigs;
 }
 
 sub filter {
     my $filtered;
 
-    for my $fh (@_) {
-	for my $line (<$fh>) {
-	    if ($line =~ /^>([^\s]+)\s+([^\s]+)/) {
-		$c1 = $1;
-		$c2 = $2;
-	    }
-	    if ($line =~ /^(\d+)\s(\d+)\s(\d+)\s(\d+)\s\d+\s\d+\s\d+$/) {
-		$contigs{$c1} = ($1,$2);
-		$contigs{$c2} = ($3,$4);
-	    }
-	}
-    }
     return $filtered;
 }
 1

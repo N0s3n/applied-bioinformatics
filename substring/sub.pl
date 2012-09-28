e52ec6e8eb81b2ddc5fd409f84d1d11884287c39
Björn Viklund <soulman_swe@hotmail.com>
2012-09-28 15:39:23 +0200

    Continued our work on the .backbone file. Problem now is the Array of array of arrays.

--- substring/sub.pl
+++ substring/sub.pl
@@ -36,11 +36,39 @@ while (<$backbonefh>) {
 
 my @substring;
 foreach (@cordArray) {
-  push(@substring, substr($joinedString,$_->[0]-1,1+($_->[1]-$_->[0])));
+  push(@substring, substr($joinedString,abs($_->[0])-1,1+abs($_->[1]-$_->[0])));
 }
 foreach (@substring) { 
-  print $_ . "\n";
+#  print $_ . "\n";
 
 }
 
 
+#parse .backbone
+
+open  $fh , "../.backbone" or die "$!";
+my $nzeroes;
+my @indices;
+my @outArray;
+my @array;
+while (<$fh>) {
+  next if /^seq0/; 
+  my @xs = split(/\s/);
+  $nzeroes = grep $_ == 0, @xs;
+  @indices = grep $xs[$_]!= 0, 0 .. $#xs;
+  if (scalar(@indices) == 2 ) {
+    #  push(@outArray[$indices[0]/2], ($xs[$indices[0]],$xs[$indices[1]]));
+    @array = [$indices[0], $indices[1]];
+    push (@outArray -> ($indices[0]/2) , @array);
+    #push (
+    #print $outArray[0][0][0] . "\n";
+    #print $array[0]->[0];
+  }
+  print $outArray[0][1][0] . "\n";
+
+
+
+  #print "$nzeroes \n";
+  #my $print = join(' ', @indices);
+  #print $print . "\n";
+}  

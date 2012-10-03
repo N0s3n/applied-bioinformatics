b399e430d6abb0680de4b877edd60e280943ee50
numfar85 <numfar85@gmail.com>
2012-10-03 15:05:49 +0200

    Added new filter function and cleaned up the code and made it fit with the new filter function.

--- pairwiseAlignment.pm
+++ pairwiseAlignment.pm
@@ -8,6 +8,7 @@ use lib do {
 };
 use Bio::Perl;
 use Data::Dumper;
+use Bio::SeqIO;
 
 sub min {
     return (sort {$a<=>$b} @_)[0]; 
@@ -51,39 +52,41 @@ sub pa {
 		close($fhdelta);
 
 		#Check positions.
-		if (not(($qi==0)and($ri==1))) {
-		    if ($pre_comb[0]==$qi) {
-			my %a = position (\%contigs_p1,\%contigs1);
-			%contigs1 =  %a;		    
-		    }
-		    elsif ($pre_comb[0]==$ri) {
-			my %b = position (\%contigs_p1,\%contigs2);
-			%contigs2 = %b;
-		    }
+		# if (not(($qi==0)and($ri==1))) {
+		#     if ($pre_comb[0]==$qi) {
+		# 	my %a = position (\%contigs_p1,\%contigs1);
+		# 	%contigs1 =  %a;		    
+		#     }
+		#     elsif ($pre_comb[0]==$ri) {
+		# 	my %b = position (\%contigs_p1,\%contigs2);
+		# 	%contigs2 = %b;
+		#     }
 
-		    if ($pre_comb[1]==$qi) {
-			my %c = position (\%contigs_p2,\%contigs1);
-			%contigs1 = %c;
-		    }
-		    elsif ($pre_comb[1]==$qi) {
-			my %d = position (\%contigs_p2,\%contigs2);
-			%contigs2 = %d;
-		    }
-		}
+		#     if ($pre_comb[1]==$qi) {
+		# 	my %c = position (\%contigs_p2,\%contigs1);
+		# 	%contigs1 = %c;
+		#     }
+		#     elsif ($pre_comb[1]==$qi) {
+		# 	my %d = position (\%contigs_p2,\%contigs2);
+		# 	%contigs2 = %d;
+		#     }
+		# }
 		
 		#Writes the new subsets to the new temp files. Then changes the names in the filename array.
 		$subQ_tempy = "sub_tempQ".$qi.$ri.".fasta"; 
 		$subR_tempy = "sub_tempR".$ri.$qi.".fasta"; 
-		open(my $fhq, ">",$subQ_tempy) 
-		    or die "cannot open < ".$subQ_tempy.": $!";
-		open(my $fhr, ">",$subR_tempy) 
-		    or die "cannot open < ".$subR_tempy.": $!";
-		print $fhq filter($filenames[$qi],\%contigs1);
-		print $fhr filter($filenames[$ri],\%contigs2);
+		# open(my $fhq, ">",$subQ_tempy) 
+		#     or die "cannot open < ".$subQ_tempy.": $!";
+		# open(my $fhr, ">",$subR_tempy) 
+		#     or die "cannot open < ".$subR_tempy.": $!";
+#		print $fhq 
+		filter($filenames[$qi],$subQ_tempy,\%contigs1);
+#		print $fhr 
+		filter($filenames[$ri],$subR_tempy,\%contigs2);
 		$filenames[$qi] = $subQ_tempy;
 		$filenames[$ri] = $subR_tempy;
-		close($fhq);
-		close($fhr);
+		# close($fhq);
+		# close($fhr);
 
 #    open(my $fhtest, ">>","test_contig_array".$qi."_".$ri.".txt") 
 #	or die "cannot open < ".$nucmer_tempy.".delta : $!";
@@ -100,7 +103,7 @@ sub pa {
     }
 
     #qx(rm -f "$nucmer_tempy.delta");
-    return \%contigs1,\%contigs2;
+    return %contigs1;
 }
 
 #function that goes through the .delta file and stores the contig ids and their positions in an array.
@@ -143,51 +146,98 @@ sub get_contigs {
     return \%contigs1,\%contigs2;
 }
 
-sub filter {
-	my ($filename, $contigs) = @_;
-	my @keys = keys(%{$contigs});
-	my $boolean = 0;
-#	for my $index (0 .. $#contigs)
-#	{
-#	    push (@keys, $contigs[$index][0]);
-#	}
+# sub filter {
+# 	my ($filename, $contigs) = @_;
+# 	my @keys = keys(%{$contigs});
+# 	my $boolean = 0;
+# #	for my $index (0 .. $#contigs)
+# #	{
+# #	    push (@keys, $contigs[$index][0]);
+# #	}
 
    
 	
-	my $found = 0;
-	my $content = "";
-	open (my $in, "<", "$filename");
-	while (my $in = <$in>)
+# 	my $found = 0;
+# 	my $content = "";
+# 	open (my $in, "<", "$filename");
+# 	while (my $in = <$in>)
+# 	{
+# 		if($in =~ /^>([^\s]+)/)
+# 		{
+# 			my $contigname = $1;
+# 			$found = 0;
+# 			foreach (@keys)
+#			{
+#				if ($_ eq $contigname)
+#				{
+#					$found = 1;
+#					$content .= $in;
+#					last();
+#				}
+#			}
+#		}
+#		elsif ($found == 1)
+#		{
+#			$content .= $in;
+#		}
+#	}
+#	close($in);
+#	return $content;
+#}
+
+sub filter {
+	my ($filename, $output, $cont) = @_;
+	my @contigs = keys (%{$cont});
+	my $fh = Bio::SeqIO->new(-file => "$filename", -format => "fasta");
+	my $filtered = Bio::SeqIO->new(-file => ">$output", -format => 'fasta' );
+	while (my $seq = $fh->next_seq)
 	{
-		if($in =~ /^>([^\s]+)/)
+		my $contigname = $seq->display_id;
+		my $hits = 0;
+		for my $cid (@contigs)
 		{
-			my $contigname = $1;
-			$found = 0;
-			foreach (@keys)
+			if ($cid eq $contigname)
 			{
-				if ($_ eq $contigname)
-				{
-					$found = 1;
-					$content .= $in;
-					last();
-				}
+				$hits++;
+				#print $contigs[$index][0]."\t".$contigname."\n";
+				#print $index . "\t" . $seq->length . "\t$contigs[$index][2]\n";
+				#my $subseq = $seq->subseq($contigs[$index][1],$contigs[$index][2]);
+				#$subseq = Bio::Seq->new(-seq => "$subseq", -id => "$contigname"."\.$hits");
+				$filtered->write_seq($seq);
 			}
 		}
-		elsif ($found == 1)
-		{
-			$content .= $in;
-		}
 	}
-	close($in);
-	return $content;
 }
 
-sub position {
-    my @pos;
-    my %c_p = %{ $_[0] };#contig information from previous iteration
-    my %c   = $_[1];#contig information from current iteration
+# sub position {
+#     my @pos;
+#     my %c_p = %{ $_[0] };#contig information from previous iteration
+#     my %c   = $_[1];#contig information from current iteration
 
+#     for my $pre (@c_p) {
 
-    return @pos;
-}
+# 	for my $cur (@c) {
+
+# 	    if ($pre->[0] eq $cur->[0]) {
+
+# 		my $startP = min($pre->[1],$pre->[2]);
+# 		my $endP = max($pre->[1],$pre->[2]);
+# 		my $rangeP = Bio::Range->new(-start => $startP, -end => $endP);
+# 		my $startC = min($cur->[1],$cur->[2]);
+# 		my $endC = max($cur->[1],$cur->[2]);
+# 		my $rangeC = Bio::Range->new(-start => $startC, -end => $endC);
+# 		my ($start,$end,$strand) = $rangeC->intersection($rangeP);
+
+# 		if(defined($start)) {
+# 		    push (@pos ,[ $pre->[0],$start,$end ]);
+# 		}
+
+# 	    }
+
+# 	}
+
+#     }
+
+#     return @pos;
+# }
 1;
